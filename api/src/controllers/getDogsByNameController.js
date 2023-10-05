fc3484d79446ef84d41a52e894006679871e8d86
Chrisnatalx <natale.christian.a@gmail.com>
2023-10-05 11:28:30 -0300

    Fix bug in search for name, in getDogByName controller, add a conditional for fixed

--- api/src/controllers/getDogsByNameController.js
+++ api/src/controllers/getDogsByNameController.js
@@ -23,14 +23,25 @@ const getDogsByNameController = async (name) => {
 			`https://api.thedogapi.com/v1/breeds/search?q=${name}&api_key=${API_KEY}`
 		)
 	).data;
+
 	const mappedDogsApi = dogsApi.map((dog) => {
-		return {
-			id: dog.id,
-			name: dog.name,
-			image: `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`,
-			temperament: dog.temperament,
-			weight: dog.weight.metric,
-		};
+		if (dog.id !== 15 && dog.id !== 212 && dog.id !== 125 && dog.id !== 66) {
+			return {
+				id: dog.id,
+				name: dog.name,
+				image: `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`,
+				temperament: dog.temperament,
+				weight: dog.weight.metric,
+			};
+		} else {
+			return {
+				id: dog.id,
+				name: dog.name,
+				image: `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.png`,
+				temperament: dog.temperament,
+				weight: dog.weight.metric,
+			};
+		}
 	});
 
 	const dogs = [...mappedDogsDb, ...mappedDogsApi];
